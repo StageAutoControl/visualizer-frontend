8b9f93eb1c6e2cddc81c114d25cc96715726201e
Alexander Pinnecke <alexander.pinnecke@googlemail.com>
2017-04-23 06:06:10 +0200

    Remove device and deviceType sub fields

--- app/dmx/data.js
+++ app/dmx/data.js
@@ -2,13 +2,13 @@ import { Device, DeviceType } from './device';
 
 export const loadDevices = () => {
   return Array
-    .from(require('./data/dmx_devices.json').dmxDevices)
+    .from(require('./data/dmx_devices.json'))
     .map(d => new Device(d));
 };
 
 
 export const loadDeviceTypes = () => {
   return Array
-    .from(require('./data/dmx_device_types.json').dmxDeviceTypes)
+    .from(require('./data/dmx_device_types.json'))
     .map(dt => new DeviceType(dt))
 };
