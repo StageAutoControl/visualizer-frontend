56732d939a6e20a91e0a80fbd6d44682b4deeaf1
Alexander Pinnecke <alexander.pinnecke@googlemail.com>
2019-02-02 20:01:22 +0100

    Fix nested objects

--- app/dmx/data.js
+++ app/dmx/data.js
@@ -2,12 +2,12 @@ import { Device, DeviceType } from './device';
 
 export const loadDevices = () => {
   return Array
-    .from(require('./data/dmx_devices.json'))
+    .from(require('./data/dmx_devices.json').dmx_devices)
     .map(d => new Device(d));
 };
 
 export const loadDeviceTypes = () => {
   return Array
-    .from(require('./data/dmx_device_types.json'))
+    .from(require('./data/dmx_device_types.json').dmx_device_types)
     .map(dt => new DeviceType(dt))
 };
